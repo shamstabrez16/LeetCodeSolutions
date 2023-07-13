58e31226960001fb5c96890d971380ea958c6663
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-07-13 15:50:43 -0500

    PalindromeNumber_9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        PalindromeNumber_9 number9 = new PalindromeNumber_9();
+        number9.isPalindrome(100);
 
-       ReverseString_344 reverseString344 = new ReverseString_344();
-        char[] s = {'H', 'e', 'l', 'l', 'o'};
-        reverseString344.reverseString(s);
+//       ReverseString_344 reverseString344 = new ReverseString_344();
+//        char[] s = {'H', 'e', 'l', 'l', 'o'};
+//        reverseString344.reverseString(s);
     }
 }
