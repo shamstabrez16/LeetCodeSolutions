8f74f8a07087c071aed4ead6adbb728a577cda7c
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-09-01 23:46:59 -0500

    11. Container With Most Water Medium 26.3K 1.4K Companies You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
    Find two lines that together with the x-axis form a container, such that the container contains the most water.
    
    Return the maximum amount of water a container can store.
    
    Notice that you may not slant the container.
    
    Example 1:
    
    Input: height = [1,8,6,2,5,4,8,3,7]
    Output: 49
    Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
    Example 2:
    
    Input: height = [1,1]
    Output: 1
    
    Constraints:
    
    n == height.length
    2 <= n <= 105
    0 <= height[i] <= 104
    Accepted
    2.3M
    Submissions
    4.3M
    Acceptance Rate
    54.1%

--- src/main/java/org/example/ContainerWithMostWater11.java
+++ src/main/java/org/example/ContainerWithMostWater11.java
@@ -5,15 +5,31 @@ import java.util.Arrays;
 public class ContainerWithMostWater11 {
 
     public static int maxArea(int[] height) {
-     int max = 0;
-        for (int i = 0; i < height.length; i++) {
-            for (int j = i+1; j <height.length ; j++) {
-                int newHeight = (j-i)* Math.min(height[i],height[j]);
-                if(newHeight>max){
-                    max= newHeight;
-                }
+        int max = 0;
+//        for (int i = 0; i < height.length; i++) {
+//            for (int j = i+1; j <height.length ; j++) {
+//                int newHeight = (j-i)* Math.min(height[i],height[j]);
+//                if(newHeight>max){
+//                    max= newHeight;
+//                }
+//            }
+//        }
+//        return max;
+        int i =0;
+        int j = height.length-1;
+        while(i<j){
+            int newHeight = (j-i)* Math.min(height[i],height[j]);
+            if(newHeight>max){
+                max= newHeight;
+            }
+            if(height[i]<height[j]){
+                i++;
+            }
+            else{
+                j--;
             }
         }
+
         return max;
 
     }
