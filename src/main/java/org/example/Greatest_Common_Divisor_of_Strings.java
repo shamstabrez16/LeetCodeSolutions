4cfe2dec8b6222ddfeb8b8725c882d4e28513bd6
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-10-11 11:28:13 -0500

    1071. Greatest Common Divisor of Strings Solved Easy Topics Companies Hint For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
    Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
    
    Example 1:
    
    Input: str1 = "ABCABC", str2 = "ABC"
    Output: "ABC"
    Example 2:
    
    Input: str1 = "ABABAB", str2 = "ABAB"
    Output: "AB"
    Example 3:
    
    Input: str1 = "LEET", str2 = "CODE"
    Output: ""
    
    Constraints:
    
    1 <= str1.length, str2.length <= 1000
    str1 and str2 consist of English uppercase letters.
    Accepted
    276K
    Submissions
    528.6K
    Acceptance Rate
    52.2%

---
+++ src/main/java/org/example/Greatest_Common_Divisor_of_Strings.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Greatest_Common_Divisor_of_Strings {
+    public static void main(String[] args) {
+        String str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX", str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
+        System.out.println(gcdOfStrings(str1, str2 ));
+    }
+    public  static String gcdOfStrings(String str1, String str2) {
+        if(str1.length()<str2.length()){
+            return gcdOfStrings(str2,str1);
+        }
+        else if(!str1.startsWith(str2)){
+          return "";
+       } else if (str2.isEmpty()) { // gcd string found.
+            return str1;
+        }else { // cut off the common prefix part of str1.
+            return gcdOfStrings(str1.substring(str2.length()), str2);
+        }
+    }
+}
